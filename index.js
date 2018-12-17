ad09b5170302f4d20d1c26a389f0b4af5f6ece3e
Robert Snyder <r.snyder49@gmail.com>
2018-12-17 00:30:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const welcomeMessage = "Welcome to Flatbook,  " + currentUser + "!";
 //OR `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser[0]}!`;
+//OR ${currentUser.slice(0, 1)}
